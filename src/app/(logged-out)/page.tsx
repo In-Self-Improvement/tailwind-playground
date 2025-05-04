dee1883bd56f32ca3c5ba3b0a5833ff1b41d8f7b
Bong <borobong230@gmail.com>
2025-05-04 20:53:51 +0900

    :wrench: [Chore] 기본 설정 추가 #main

---
+++ src/app/(logged-out)/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export default function LoggedOut() {
+  return (
+    <div>
+      <h1>Support Me</h1>
+      <Button>Login</Button>
+      <Button>Sign up</Button>
+    </div>
+  );
+}
