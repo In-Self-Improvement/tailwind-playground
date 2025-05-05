eed70c4f88d197e1680e182933e89f2b10dcbace
Bong <borobong230@gmail.com>
2025-05-05 09:25:08 +0900

    :sparkles: [Feat] 로그인 페이지 기본 ui 추가 #main

---
+++ src/app/(logged-out)/layout.tsx
@@ -0,0 +1,11 @@
+interface LoggedOutLayoutProps {
+  children?: React.ReactNode;
+}
+
+export default function LoggedOutLayout({ children }: LoggedOutLayoutProps) {
+  return (
+    <div className="flex flex-col gap-4 min-h-screen items-center justify-center p-24">
+      {children}
+    </div>
+  );
+}
--- src/app/(logged-out)/page.tsx
+++ src/app/(logged-out)/page.tsx
@@ -1,11 +1,26 @@
 import { Button } from "@/components/ui/button";
+import { PersonStandingIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function LoggedOut() {
   return (
-    <div>
-      <h1>Support Me</h1>
-      <Button>Login</Button>
-      <Button>Sign up</Button>
-    </div>
+    <>
+      <h1 className="flex gap-2 items-center">
+        <PersonStandingIcon size={50} className="text-pink-500" />
+        Support Me
+      </h1>
+      <p className="text-muted-foreground">
+        The best way to support me is to donate to my cause.
+      </p>
+      <div className="flex gap-2 items-center">
+        <Button asChild>
+          <Link href="/login">Login</Link>
+        </Button>
+        <span>or</span>
+        <Button variant="outline" asChild>
+          <Link href="/sign-up">Sign up</Link>
+        </Button>
+      </div>
+    </>
   );
 }
